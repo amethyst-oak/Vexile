7e11b7fb4cf2bae50a159137cfd3dbaccdde44f3
completedev <thirdplay6@gmail.com>
2026-07-23 13:21:26 +0300

    Update build.sh

--- Vexile/src/build.sh
+++ Vexile/src/build.sh
@@ -1,8 +1,8 @@
 /usr/lib/jvm/java-8-openjdk-amd64/bin/javac -source 1.8 -target 1.8 -encoding UTF-8 -d build/classes VexileLauncher.java
 
-# 2. Создаем манифест
-echo "Manifest-Version: 1.0" > build/manifest.txt
+# Creating a manifest
+echo "Manifest-Version: 1.2" > build/manifest.txt
 echo "Main-Class: VexileLauncher" >> build/manifest.txt
 
-# 3. Упаковываем в jar
+# Do in jar
 jar cfm dist/VexileLauncher.jar build/manifest.txt -C build/classes .
